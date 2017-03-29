e2bdb122abfdf384162bc5cab8c8747dd19f3545
Alexander Haniotis <alexanderhaniotis@gmail.com>
2017-03-29 10:55:10 -0400

    Animations

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,11 +1,5 @@
-import FastClick from 'fastclick'
-
-// Fastclick
-FastClick.attach(document.body)
-
-// Mobile nav
-document.querySelector('.js-toggle-nav').addEventListener('click', () => {
-  document.querySelector('body').classList.toggle('is-clipped')
-  document.querySelector('.js-mobile-nav').classList.toggle('is-open')
-  document.querySelector('.js-hamburger').classList.toggle('is-open')
-})
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.js-page').addClass('is-loaded');
+  }, 1200);
+});
